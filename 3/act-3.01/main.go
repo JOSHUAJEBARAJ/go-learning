bb1b5bfc34c11e280c997b903ffde9997815341c
Joshua <joshua@Joshuas-MacBook-Pro.local>
2021-02-05 16:24:41 +0530

    Added 3rd

--- 3/act-3.01/main.go
+++ 3/act-3.01/main.go
@@ -1,12 +1,12 @@
 package main
 import "fmt"
 func main()  {
-	var at float32=0.075
-	var bt float32=0.2
-	var mt float32 = 0.015
-	var a float32=0.99
-	var m float32=2.75
-	var b float32 = 0.87
+	var at float32=0.075 // cake tax
+	var bt float32=0.02 // butter tax
+	var mt float32 = 0.015 //milk tax
+	var a float32=0.99 // cake
+	var m float32=2.75 // milk
+	var b float32 = 0.87 // butter
 	var total float32
 	total=(at*a)+(bt*b)+(mt*m)
 	fmt.Println(total)
